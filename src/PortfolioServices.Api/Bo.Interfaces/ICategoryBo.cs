b9b7ee2303b2e7a66b99bc54511c2e8e5194ebc9
TinhTienTi <49122993+TinhTienTi@users.noreply.github.com>
2023-03-11 10:40:13 +0700

    feat<CategoryBo>:Add CategoryBo & ICategoryBo

---
+++ src/PortfolioServices.Api/Bo.Interfaces/ICategoryBo.cs
@@ -0,0 +1,13 @@
+﻿using PortfolioServices.Dto;
+
+namespace PortfolioServices.Api.Bo.Interfaces
+{
+    public interface ICategoryBo
+    {
+        Task<CategoriesDto> CreateAsync(CategoriesDto home);
+
+        Task<IEnumerable<CategoriesDto>> GetAsync();
+
+        Task<CategoriesDto> GetAsync(Guid Tid);
+    }
+}
